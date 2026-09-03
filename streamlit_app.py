e3be7b89b08404917f83a4c45032874c81378ecd
slesperance1234 <slesperance@gmail.com>
2026-09-03 07:09:43 -0400

    Remove commented out balloon data entries

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,9 +51,7 @@ defaut_passagers = pd.DataFrame([
   {"contrat": "185698", "poids": 165},
   {"contrat": "185698", "poids": 195},
   {"contrat": "185698", "poids": 113},
- //   {"contrat": "180759", "poids": 118},
- //   {"contrat": "180759", "poids": 160},
-])
+ ])
 df_passagers = st.data_editor(defaut_passagers, num_rows="dynamic", width=300)
 
 # Convertir en listes de dictionnaires
